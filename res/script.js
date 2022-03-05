4a466296c6dca8979c89ae2dd788ba9c6e89c1ab
IndipendenteTrockista <indipendentetrockista@gmail.com>
2022-03-05 22:46:17 +0100

    5 Marzo 2022 #3

---
+++ res/script.js
@@ -0,0 +1,8 @@
+function showList() {
+    list = document.getElementById("sfogliaList");
+    if ( list.style.display == "block" ) {
+        list.style.display = "none";
+    } else {
+        list.style.display = "block";
+    }
+}
