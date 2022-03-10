18e9d6b68cb2288355d27699e5ee8cc1d760fdbe
IndipendenteTrockista <indipendentetrockista@gmail.com>
2022-03-10 23:29:01 +0100

    10 Marzo 2022 #3

--- res/script.js
+++ res/script.js
@@ -11,3 +11,17 @@ function goToArticleFromPreview( articleName ) {
 	var year = new Date().getFullYear();
 	window.location.href = "articoli/" + year + "/" + articleName + ".html";
 }
+
+function goToPageFromLink( postDestination ) {
+	var href = window.location.href;
+	var hrefArr = href.split("/");
+	var location = hrefArr[3];
+	var preDestination = "";
+	if ( location == "speciali" ) {
+		preDestination = "../";
+	} else if ( location == "articoli" ) {
+		preDestination == "../../../";
+	}
+	var destination = preDestination + postDestination;
+	window.location.href = destination;
+}
